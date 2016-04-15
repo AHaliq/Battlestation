d91d36d7b4487ece751e7689bf8e559e648909f5
AHaliq <liqo12@hotmail.com>
2016-04-15 21:05:24 +0800

    install git & install yosembiance & update unity tweak tool prompt message

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
+#install git
+sudo apt-get install git
 #install numix icon theme
 sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
 sudo apt-get install numix-icon-theme-circle
+#clone yosembiance theme
+if [-d "~/.themes"]; then
+mkdir ~/.themes
+fi
+git clone https://github.com/bsundman/Yosembiance.git ~/.themes
 #unity-tweak-tool > appearance > icons > Numix-circle
 sudo apt-get install unity-tweak-tool
-echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo navigate to Appearance, Icons, Numix-Circle
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo set Icons to : Numix-Circle, Themes to : Yosembiance-atomic-blue
 unity-tweak-tool
-echo good
-echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
