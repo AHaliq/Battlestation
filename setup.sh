b9972da48ac32fc2549135c0c09dbc8bb5cd8af9
AHaliq <liqo12@hotmail.com>
2016-04-15 20:42:10 +0800

    install unity tweak tool and request user to change icon theme

--- setup.sh
+++ setup.sh
@@ -2,5 +2,11 @@
 sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
 sudo apt-get install numix-icon-theme-circle
-
+#unity-tweak-tool > appearance > icons > Numix-circle
+sudo apt-get install unity-tweak-tool
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo navigate to Appearance, Icons, Numix-Circle
+unity-tweak-tool
+echo good
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
