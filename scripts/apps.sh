d187deb034a05efaaab4c47cc9c6bbdf4e4cddce
AHaliq <liqo12@hotmail.com>
2016-04-16 09:13:36 +0800

    copy config files after install

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -10,12 +10,10 @@ sudo apt-get install git
 #install zsh
 sudo apt-get install zsh
 wget https://github.com/robbyrussel/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-#copy over .zshrc
+cp ./configfiles/.zshrc ~/
 chsh -s /usr/bin/zsh
 
 
 #install tmux
 sudo apt-get install tmux
-#copy over .tmux.conf
-
-
+cp ./configfiles/.tmux.conf ~/
