6803707f37cbc7b33f43fd0a8ac7d46df158b95d
AHaliq <liqo12@hotmail.com>
2016-04-16 10:47:59 +0800

    removed old powerline installation commands from

--- setup.sh
+++ setup.sh
@@ -12,23 +12,5 @@ fc-cache -f -v
 sh ./scripts/apps.sh
 sh ./scripts/vimsetup.sh
 
-#install powerline
-sudo apt-get install python-pip
-pip install --user powerline-status
-pip install --user git+git://github.com/powerline/powerline
-# zsh powerline
-echo "# Powerline Configuration" >> ~/.zshrc
-echo ". ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh" >> ~/.zshrc
-# vim powerline
-echo '" Powerline configuration' | sudo tee --append /etc/vim/vimrc
-echo "python from powerline.vim import setup as powerline_setup" | sudo tee --append /etc/vim/vimrc
-echo "python powerline_setup()" | sudo tee --append /etc/vim/vimrc
-echo "pythong del powerline_setup" | sudo tee --append /etc/vim/vimrc
-echo "set laststatus=2" | sudo tee --append /etc/vim/vimrc
-echo "set t_Co=256" | sudo tee --append /etc/vim/vimrc
-# tmux powerline
-tmux show -g | cat > ~/.tmux.conf
-echo "source ~/.local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf" >> ~/.tmux.conf
-
 sudo reboot
 
