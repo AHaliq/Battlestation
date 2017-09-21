b26c65b5c7241f008838c680d88990a9f893b23c
AHaliq <liqo12@hotmail.com>
2017-09-22 02:52:55 +0800

    updated todos

--- src/os/osx/install.sh
+++ src/os/osx/install.sh
@@ -5,6 +5,7 @@ OSXPORT=""
 # VARS ==========================================
 
 echo " ${CBLU} OSX ${C}version ${CHI}${OSXVER}${C}"
+# greet
 
 if [[ $OSXVER == "10.11"* ]]; then
     OSXPORT="https://github.com/macports/macports-base/releases/download/v2.4.1/MacPorts-2.4.1-10.11-ElCapitan.pkg"	
@@ -25,5 +26,11 @@ else
 fi
 # check valid download
 
-# TODO: detect 
+# TODO: try to install macports, see pkgutil --pkgs, --files, compare with 
+# https://guide.macports.org/#installing.macports.uninstalling
+# make uninstaller script, get comfortable repeatedly install and uninstall macports
+# check macports existence before download, skip if exists
+# pkg removal guide: <i dont think i want to do removal script proper, uninstall only for ensuring installer works>
+# https://stackoverflow.com/questions/25925752/uninstall-packages-in-mac-os-x
+# TODO vagrant, then proper shell customization, then vim, then continue C or more dev env stuff
 # install
