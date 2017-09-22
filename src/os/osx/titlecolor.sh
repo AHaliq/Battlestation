d9973056c9a4c6047daa28034ff25e4698f24900
AHaliq <liqo12@hotmail.com>
2017-09-22 17:01:55 +0800

    changed bg color to match osx windows

--- src/os/osx/titlecolor.sh
+++ src/os/osx/titlecolor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -en "\033]6;1;bg;red;brightness;241\a"
-echo -en "\033]6;1;bg;green;brightness;241\a"
-echo -en "\033]6;1;bg;blue;brightness;241\a"
+echo -en "\033]6;1;bg;red;brightness;236\a"
+echo -en "\033]6;1;bg;green;brightness;236\a"
+echo -en "\033]6;1;bg;blue;brightness;236\a"
