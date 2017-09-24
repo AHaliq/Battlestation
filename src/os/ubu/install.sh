c8b039bd385cf0a4845949e74c3f196ab9f12750
AHaliq <liqo12@hotmail.com>
2017-09-24 16:40:16 +0000

    apt-get update

--- src/os/ubu/install.sh
+++ src/os/ubu/install.sh
@@ -4,3 +4,6 @@ UBU_VER="$(lsb_release -rs)"
 # VARS ==========================================
 
 echo "$CTI UBUNTU $C version $CHI$UBU_VER$C"
+
+echo "${TCB} update apt-get"
+sudo apt-get -yqq update
