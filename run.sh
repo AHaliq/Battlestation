9f615f35cbd7ccee8245c4bd11d63f82304b4a35
AHaliq <liqo12@hotmail.com>
2017-09-24 16:46:13 +0800

    extract main loop out of initial run script

--- run.sh
+++ run.sh
@@ -6,29 +6,7 @@ TDIR=$(cd $(dirname "$0") && pwd -P)
 DDIR="${TDIR}/bin"
 # VARS ==========================================
 
-source "${TDIR}/src/header.sh"
-echo "${CTI} !BATTLESTATION! $C"
-# start
-
-if [[ $UNME == 'Darwin' ]]; then
-    UNME='osx'
-else
-    echo "${TERR}unexpected os ${CHI}${UNME}${C}"
-    exit 1
-fi
-# determine os
-
-cd "${TDIR}/src/os/${UNME}"
-source "./install.sh" 
-# run os specific installation
-
-installapp () { source "./install.sh"; }
-for D in $(find "${TDIR}/src/app" -d 1 -type d); do
-  cd $D
-  query "${D##*/}" installapp
-done
-# loop src/app dirs, go in, call install
-
-echo "${CTI} success $C"
-cd $SDIR
-# return to source directory
+source "${TDIR}/src/main/header.sh"
+source "${TDIR}/src/main/install.sh"
+#source "${TDIR}/src/os/osx/uninstall/uninstallctl.sh"
+#source "${TDIR}/src/os/osx/uninstall/uninstallmacports.sh"
---
+++ src/main/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "${CTI} !BATTLESTATION! $C"
+# start
+
+if [[ $UNME == 'Darwin' ]]; then
+    UNME='osx'
+else
+    echo "${TERR}unexpected os ${CHI}${UNME}${C}"
+    exit 1
+fi
+# determine os
+
+cd "${TDIR}/src/os/${UNME}"
+source "./install.sh" 
+# run os specific installation
+
+installapp () { source "./install.sh"; }
+for D in $(find "${TDIR}/src/app" -d 1 -type d); do
+  cd $D
+  query "${D##*/}" installapp
+done
+# loop src/app dirs, go in, call install
+
+echo "${CTI} success $C"
+cd $SDIR
+# return to source directory
