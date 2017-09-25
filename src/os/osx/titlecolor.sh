984d70a2f4e271b3a55d0d40ae71161af3e33f05
AHaliq <liqo12@hotmail.com>
2017-09-25 17:37:41 +0800

    updated color scheme

--- src/os/osx/titlecolor.sh
+++ src/os/osx/titlecolor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -en "\033]6;1;bg;red;brightness;236\a"
-echo -en "\033]6;1;bg;green;brightness;236\a"
-echo -en "\033]6;1;bg;blue;brightness;236\a"
+echo -en "\033]6;1;bg;red;brightness;43\a"
+echo -en "\033]6;1;bg;green;brightness;48\a"
+echo -en "\033]6;1;bg;blue;brightness;59\a"
