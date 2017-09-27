86eb492a46c0c17cd531132c1d91d9ae8cb7b653
AHaliq <liqo12@hotmail.com>
2017-09-27 16:45:08 +0800

    interface for vim installer, iterm theme update

---
+++ src/app/vim/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#detect vim7 query to remove it, source uninstall script
+#/usr/share/vim
+#/usr/bin/vim
+
+#detect vim8
+#install
+# libncurses5-dev
+# libgnome2-dev
+# libgnomeui-dev
+# libgtk2.0-dev
+# libatk1.0-dev
+# libbonoboui2-dev
+# libcairo2-dev
+# libx11-dev
+# libxpm-dev
+# libxt-dev
+# python-dev
+# python3-dev
+# ruby-dev
+# lua5.1
+# lua5.1-dev
+# libperl-dev
+#install make
